1288e466785998025737fc51099f7742dd2d6647
Brett Kaplan <BrettCKaplan@gmail.com>
2012-11-16 01:56:18 -0500

    set up a way to change stuff by pausing/restarting game

--- src/main.py
+++ src/main.py
@@ -1,12 +1,7 @@
 #Brett Kaplan
 #November 12, 2012
-
 import pygame, sys, math, random
-
 random.seed()
-SCREEN_WIDTH = 256
-SHIFT_AMOUNT = 8
-FRAMERATE = 60
 
 class Pixel(object):
     def __init__(self, setx, sety, setr, setg, setb):
@@ -24,15 +19,13 @@ class Pixel(object):
         
 class Visual(object):
     def __init__(self, setw, seth):
-        self.screen = pygame.display.set_mode((setw, seth))#, pygame.FULLSCREEN)
-        #self.screen = pygame.display.set_mode((setw, seth))#, pygame.NOFRAME) perhaps
         self.w = setw
-        self.h = seth
-        self.pixel_sets = [[[Pixel(x, y, max(0,(x-y)*256/SCREEN_WIDTH), max(0,(x+y-SCREEN_WIDTH)*256/SCREEN_WIDTH), max(0,(y-x)*256/SCREEN_WIDTH)) for y in range(seth)] for x in range(setw)] for n in range(2)]
+        #self.h = seth
+        self.screen = pygame.display.set_mode((self.w, self.w))
+        self.pixel_sets = [[[Pixel(x, y, max(0,(x-y)*256/self.w), max(0,(x+y-self.w)*256/self.w), max(0,(y-x)*256/self.w)) for y in range(self.w)] for x in range(self.w)] for n in range(2)]
         self.current_set = 0
         self.awaiting_update = [dict() for n in range(2)]#list of dicts; (x,y):s
         self.awaiting_draw = sum( self.pixel_sets[self.current_set] ,[])
-        #self.draw()
         
     def update(self, t):
         if len(self.awaiting_update[self.current_set]) > 0:
@@ -40,7 +33,7 @@ class Visual(object):
                 if shift == 0:
                     continue
                 self.awaiting_draw.append(self.pixel_sets[not self.current_set][pxy[0]][pxy[1]].shiftColor(self.pixel_sets[self.current_set][pxy[0]][pxy[1]].color, shift))
-                txy = ((pxy[0]-1)%SCREEN_WIDTH,pxy[1])
+                txy = ((pxy[0]-1)%self.w,pxy[1])
                 if txy in self.awaiting_update[not self.current_set]:
                     if self.awaiting_update[not self.current_set][txy] > 0:
                         #self.awaiting_update[not self.current_set][txy] = (self.awaiting_update[not self.current_set][txy]+math.fabs(shift))/-2#shift, maybe no fabs
@@ -50,7 +43,7 @@ class Visual(object):
                 else:
                     self.awaiting_update[not self.current_set][txy] = shift/2#math.fabs(shift)/2
                 
-                txy = ((pxy[0]+1)%SCREEN_WIDTH,pxy[1])
+                txy = ((pxy[0]+1)%self.w,pxy[1])
                 if txy in self.awaiting_update[not self.current_set]:
                     if self.awaiting_update[not self.current_set][txy] > 0:
                         #self.awaiting_update[not self.current_set][txy] = (self.awaiting_update[not self.current_set][txy]+math.fabs(shift))/-2#shift, maybe no fabs
@@ -60,7 +53,7 @@ class Visual(object):
                 else:
                     self.awaiting_update[not self.current_set][txy] = shift/2#math.fabs(shift)/2
                 
-                txy = (pxy[0],(pxy[1]-1)%SCREEN_WIDTH)
+                txy = (pxy[0],(pxy[1]-1)%self.w)
                 if txy in self.awaiting_update[not self.current_set]:
                     if self.awaiting_update[not self.current_set][txy] > 0:
                         #self.awaiting_update[not self.current_set][txy] = (self.awaiting_update[not self.current_set][txy]+math.fabs(shift))/-2#shift, maybe no fabs
@@ -70,7 +63,7 @@ class Visual(object):
                 else:
                     self.awaiting_update[not self.current_set][txy] = shift/2#math.fabs(shift)/2
                 
-                txy = (pxy[0],(pxy[1]+1)%SCREEN_WIDTH)
+                txy = (pxy[0],(pxy[1]+1)%self.w)
                 if txy in self.awaiting_update[not self.current_set]:
                     if self.awaiting_update[not self.current_set][txy] > 0:
                         #self.awaiting_update[not self.current_set][txy] = (self.awaiting_update[not self.current_set][txy]+math.fabs(shift))/-2#shift, maybe no fabs
@@ -88,21 +81,49 @@ class Visual(object):
         self.awaiting_draw = []
         pygame.display.flip()
         
-on = True
-pygame.init()
-my_visual = Visual(SCREEN_WIDTH,SCREEN_WIDTH)
-my_clock = pygame.time.Clock()
+class Game(object):
+    def __init__(self, scr_width):
+        pygame.init()
+        self.my_visual = Visual(scr_width,scr_width)
+        self.my_clock = pygame.time.Clock()
+        
+    def __del__(self):
+        pygame.quit()
+        
+    def run(self, shft_amt, frmrt):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return 0
+                if event.key == pygame.K_m:
+                    return 2
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mxy = event.pos
+                self.my_visual.awaiting_update[self.my_visual.current_set][mxy] = shft_amt
+        self.my_visual.update(self.my_clock.tick(frmrt))
+        self.my_visual.draw()
+        return 1
+        
+SCREEN_WIDTH = 256
+SHIFT_AMOUNT = 8
+FRAMERATE = 60
+my_game = Game(SCREEN_WIDTH)
+on = 1
 while on:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            on = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                on = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mxy = event.pos
-            my_visual.awaiting_update[my_visual.current_set][mxy] = SHIFT_AMOUNT
-    my_visual.update(my_clock.tick(FRAMERATE))
-    my_visual.draw()
-pygame.quit()
+    if on == 1:
+        on = my_game.run(SHIFT_AMOUNT, FRAMERATE)
+    elif on == 2:
+        del my_game
+        new_width = 401
+        while new_width > 400 or new_width < 4:
+            try:
+                new_width = int(raw_input('New screen width (less than 400):'))
+            except ValueError:
+                new_width = 401
+        my_game = Game(new_width)
+        on = my_game.run(SHIFT_AMOUNT, FRAMERATE)
+    
+del my_game
 sys.exit()
